7a825c64b39438209f0ca168a805a8ebd470c1cf
Evgeny Stepanischev <imbolk@gmail.com>
2024-12-04 18:58:53 +0300

    mime-type → mime

--- http-formatted.sh
+++ http-formatted.sh
@@ -8,7 +8,7 @@ while :;do
         [ ${#b} -le 1 ]&&{
             $E "HTTP/1.0 200\n$C-length:"
             if [ -f "$k" ]; then
-                $E "`wc -c<"$k"`\n$C-type:`file -b --mime-type "$k"`\n\n";cat "$k"
+                $E "`wc -c<"$k"`\n$C-type:`file -b --mime "$k"`\n\n";cat "$k"
             else
                 l=`$E "<a href=..><<</a><hr><ul>";ls -1F "$k"|sed 's/\(.*\)/<li><a href="\1">\1<\/a><br>/'`
                 $E "${#l}\n$C-type:text/html;charset=utf-8\n\n$l\n"
