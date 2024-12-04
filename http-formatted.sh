b72089c24a017538a60cd92644fe2ae2bffca46b
Evgeny Stepanischev <imbolk@gmail.com>
2024-12-04 18:40:27 +0300

    '+' does not need to be processed

--- http-formatted.sh
+++ http-formatted.sh
@@ -2,19 +2,19 @@ while :;do
     E=echo\ -ne;C=content
     coproc p { nc -l 8080; }
 
-    while read -r a b x; do
-        [ ! "$f" ]&&f=".$($E ${b//%/\\x}|sed 'y/+/ /')"
+    while read -r b o l; do
+        [ ! "$k" ]&&k=".$($E ${o//%/\\x})"
 
-        [ ${#a} -le 1 ]&&{
+        [ ${#b} -le 1 ]&&{
             $E "HTTP/1.0 200\n$C-length:"
-            if [ -f "$f" ]; then
-                $E "`wc -c<"$f"`\n$C-type:`file -b --mime-type "$f"`\n\n";cat "$f"
+            if [ -f "$k" ]; then
+                $E "`wc -c<"$k"`\n$C-type:`file -b --mime-type "$k"`\n\n";cat "$k"
             else
-                x=`$E "<a href=..><<</a><hr><ul>";ls -1F "$f"|sed 's/\(.*\)/<li><a href="\1">\1<\/a><br>/'`
-                $E "${#x}\n$C-type:text/html;charset=utf-8\n\n$x\n"
+                l=`$E "<a href=..><<</a><hr><ul>";ls -1F "$k"|sed 's/\(.*\)/<li><a href="\1">\1<\/a><br>/'`
+                $E "${#l}\n$C-type:text/html;charset=utf-8\n\n$l\n"
             fi
 
-            f=
+            k=
         }>&${p[1]}
     done<&${p[0]}
 done
